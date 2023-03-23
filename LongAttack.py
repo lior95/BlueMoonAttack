c75a0e4a2de3467b90e8d615b703724974e8e80f
lior <lior.levi89@gamail.com>
2023-03-23 13:58:06 +0200

    init (LongAttack):create init fun

---
+++ LongAttack.py
@@ -0,0 +1,7 @@
+from Attack import Attack
+
+class LongAttack(Attack):
+
+    def __init__(self, angleOfRotation, angleToGround, recoveryTime):
+        super(angleOfRotation, recoveryTime)
+        self.angleToGround = angleToGround
