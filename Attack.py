448ffe60ecacceff5fe1c8b71cf39c5b2a5c3b30
lior <lior.levi89@gamail.com>
2023-03-23 13:44:22 +0200

    feat (Attack):create init and get func

---
+++ Attack.py
@@ -0,0 +1,15 @@
+
+class Attack():
+
+    def __init__(self, angleOfRotation, recoveryTime):
+        self.angleOfRotation = angleOfRotation
+        self.recoveryTime = recoveryTime
+
+    def getAngleOfRotation(self):
+        self.angleOfRotation
+
+    def getRecoveryTime(self):
+        self.recoveryTime
+
+
+
